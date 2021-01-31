46cf55f3a427d63d98a81ca78752102a5b59902b
Yuta <y.sugiyama54@gmail.com>
2021-01-31 17:12:43 +0900

    ORマッパーの実装

--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.login.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
@@ -11,6 +12,7 @@ import com.example.demo.login.domain.repository.UserDao;
 public class UserService
 {
 	@Autowired
+	@Qualifier("UserDaoJdbcImpl4")
 	UserDao dao;
 	
 	// insert用のメソッド
